6c36e2cdb4926e61e0c5d06527049ab47e56041d
Noah Ramey <noahramey1997@gmail.com>
2016-07-15 13:47:13 -0700

    add a bit of routing to app.rb, haven't written specs as of yet

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require('./lib/client')
 require('./lib/stylist')
 require('pg')
 
-DB = PG.connect({:dbname => "hair_salon_test"})
+DB = PG.connect({:dbname => "hair_salon"})
 
 get('/') do
   erb(:index)
@@ -22,9 +22,49 @@ get('/stylists') do
 end
 
 get('/clients/new') do
+  @stylists = Stylist.all()
   erb(:client_form)
 end
 
 get('/stylists/new') do
   erb(:stylist_form)
+end
+
+post('/clients') do
+  name = params.fetch('name')
+  stylist_id = params.fetch('stylist_id').to_i()
+  client = Client.new({name: name, stylist_id: stylist_id})
+  client.save()
+  @clients = Client.all()
+  erb(:clients)
+end
+
+post('/stylists') do
+  name = params.fetch('name')
+  stylist = Stylist.new({name: name})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/clients/:id') do
+  @client = Client.find(params.fetch('id').to_i())
+  erb(:client)
+end
+
+get('/stylists/:id') do
+  @stylist = Stylist.find(params.fetch('id').to_i())
+  erb(:stylist)
+end
+
+get("/clients/:id/edit") do
+  @client = Client.find(params.fetch("id").to_i())
+  @stylists = Stylist.all()
+  erb(:client_edit)
+end
+
+get("/stylists/:id/edit") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @clients = Client.all()
+  erb(:stylist_edit)
 end
